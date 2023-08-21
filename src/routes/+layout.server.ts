2beb31df185e574b0cbdd83ca9ff3cc3dbfe4c6a
Ruter Lyu <ruter.lv@gmail.com>
2023-08-21 12:51:46 +0800

    fix ZLIB_1.2.9 not found

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,17 @@
+import { building } from '$app/environment';
+import { env } from '$env/dynamic/private';
+
+if (building) {
+    console.log(env.LD_LIBRARY_PATH);
+    // For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+    if (
+    env.LD_LIBRARY_PATH == null ||
+    !env.LD_LIBRARY_PATH.includes(
+        `${env.PWD}/node_modules/canvas/build/Release:`,
+    )
+    ) {
+    env.LD_LIBRARY_PATH = `${
+        env.PWD
+    }/node_modules/canvas/build/Release:${env.LD_LIBRARY_PATH || ''}`;
+    }
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
